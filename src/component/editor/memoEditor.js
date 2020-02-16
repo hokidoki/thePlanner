f0d5000efabd04cb42a7d0e9ee747dc96d7404e4
hokey_imac <substantiation@naver.com>
2020-02-17 01:32:15 +0900

    image Handler - done
    image resize error - done
    image file_size_limit - done
    image total_file_size_limit - done

--- src/component/editor/memoEditor.js
+++ src/component/editor/memoEditor.js
@@ -3,32 +3,57 @@ import ReactQuill, { Quill } from 'react-quill';
 import '../../style/quill.snow.css'
 import ImageResize from 'quill-image-resize-module';
 Quill.register('modules/ImageResize', ImageResize);
+// Quill.on('text-change', function(delta, oldDelta, source) {
+//   // if (source == 'api') {
+//   //   console.log("An API call triggered this change.");
+//   // } else if (source == 'user') {
+//   //   console.log("A user action triggered this change.");
+//   // }
+//   console.log(delta);
+//   console.log(oldDelta);
+//   console.log(source);
+// });
 
 export default class MemoEditor extends Component {
 
-<<<<<<< Updated upstream
 
   imageHandleChange(){
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
-    input.click();
-    input.onchange = function () {
-      const file = input.files[0];
-      const imgMaxSize = 1 * 1024 * 1024; // 2MB
-      const reader = new FileReader();
-      if (file.size < imgMaxSize) {
-        reader.readAsDataURL(file);
-        console.log(file)
-        reader.onload = () => {
-          const cursorPosition = this.quill.getSelection().index;
-          this.quill.insertEmbed(cursorPosition, 'image', reader.result)
+    const imgTags = document.querySelectorAll('.quill img');
+    const imgTagsLength = imgTags.length;
+    const totalImgMaxSize = 5 * 1024 * 1024;
+    let imgTagsTotalSize = 0;
+    
+    for(let i = 0; i < imgTagsLength; i++){
+      const fileSize = (imgTags[i].src.length * (3/4)) -2; 
+      imgTagsTotalSize += fileSize;
+    }
+    
+    if( imgTagsLength <= 4){
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+      input.click();
+      input.onchange = function () {
+        const file = input.files[0];
+        if(imgTagsTotalSize + file.size < totalImgMaxSize){
+          const imgMaxSize = 4 * 1024 * 1024; // 2MB
+          const reader = new FileReader();
+          if (file.size < imgMaxSize) {
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+              const cursorPosition = this.quill.getSelection().index;
+              this.quill.insertEmbed(cursorPosition, 'image', reader.result);
+          }}else{
+          alert("이미지는 2MB 이하만 가능합니다.");
+          return;
+        }}else{
+          alert("이미지의 총 파일 크기는 10mb를 넘지 못합니다. ");
         }
-      } else {
-        alert("이미지는 2MB 이하만 가능합니다.");
-        return;
-      }
-    }.bind(this)
+      }.bind(this)
+    }else{
+      alert("이미지는 5개 까지만 업로드 가능합니다.");
+      return;
+    }
   }
 
   state = {
@@ -71,60 +96,24 @@ export default class MemoEditor extends Component {
     ]
   }
 
-=======
-  state = {
-    theme: 'snow',
-    modules: {
-      toolbar: [
-        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-        [{ size: [] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' },
-        { 'indent': '-1' }, { 'indent': '+1' }],
-        ['link', 'image', 'video'],
-        ['clean']
-      ],
-      imageResize: {
-        handleStyles: {
-          backgroundColor: 'black',
-          border: 'none',
-          color: 'white',
-          // other camelCase styles for size display
-        },
-        displayStyles: {
-          backgroundColor: 'black',
-          border: 'none',
-          color: 'white',
-        },
-        modules: ['Resize', 'DisplaySize', 'Toolbar']
-      }
-      ,
-      clipboard: {
-        // toggle to add extra line breaks when pasting HTML:
-        matchVisual: false,
-      }
-    },
-    format: [
-      'header', 'font', 'size',
-      'bold', 'italic', 'underline', 'strike', 'blockquote',
-      'list', 'bullet', 'indent',
-      'link', 'image', 'video'
-    ]
-  }
-
->>>>>>> Stashed changes
-  handleChange = (html) => {
+  
+  handleChange = (html,delta,source,editor) => {
     this.props.handleChange(html);
+    // console.log(editor.getContents())
+    // console.log(delta);
+    // console.log(source)
   }
 
-<<<<<<< Updated upstream
+  // dummy(){
+  //   console.log(this.getContents());
+  // }
+
  
-=======
->>>>>>> Stashed changes
   render() {
     return (
       <div className="editorContainer">
         <ReactQuill
+          ref='editor'
           theme={this.state.theme}
           onChange={this.handleChange}
           value={this.props.editorHtml}
